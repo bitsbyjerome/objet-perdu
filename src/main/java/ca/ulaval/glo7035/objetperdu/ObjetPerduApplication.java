e70dc0e5c84ceb5ab30122bcca0a641be7b8b096
jean <jeanrolsen@gmail.com>
2018-11-08 23:08:21 -0500

    Initial Commit

---
+++ src/main/java/ca/ulaval/glo7035/objetperdu/ObjetPerduApplication.java
@@ -0,0 +1,12 @@
+package ca.ulaval.glo7035.objetperdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetPerduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetPerduApplication.class, args);
+	}
+}
---
+++ src/test/java/ca/ulaval/glo7035/objetperdu/ObjetPerduApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.ulaval.glo7035.objetperdu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ObjetPerduApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
